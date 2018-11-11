8e06fcf8ae9981e05969dda063fc2ffddb9f5018
Nikolas Tay <niktay94@gmail.com>
2018-11-12 01:58:46 +0800

    Implement Post deleting

--- cheddar/blog/schema.py
+++ cheddar/blog/schema.py
@@ -1,3 +1,5 @@
+from copy import deepcopy
+
 import graphene
 from blog.models import Post
 from graphene import InputObjectType
@@ -9,14 +11,14 @@ class PostType(DjangoObjectType):
         model = Post
 
 
-class PostInput(InputObjectType):
+class CreatePostInput(InputObjectType):
     title = graphene.String(required=True)
     content = graphene.String(required=True)
 
 
 class CreatePost(graphene.Mutation):
     class Arguments:
-        post_data = PostInput(required=True)
+        post_data = CreatePostInput(required=True)
 
     post = graphene.Field(PostType)
 
@@ -25,6 +27,26 @@ class CreatePost(graphene.Mutation):
         post = Post(title=post_data.title, content=post_data.content)
         post.save()
         return CreatePost(post=post)
+
+
+class DeletePostInput(InputObjectType):
+    id = graphene.ID(required=True)
+
+
+class DeletePost(graphene.Mutation):
+    class Arguments:
+        post_data = DeletePostInput(required=True)
+
+    post = graphene.Field(PostType)
+
+    @staticmethod
+    def mutate(root, info, post_data=None):
+        post_to_delete = Post.objects.get(pk=post_data.id)
+        deleted_post = deepcopy(post_to_delete)
+
+        post_to_delete.delete()
+
+        return DeletePost(post=deleted_post)
 
 
 class Query(object):
@@ -36,3 +58,4 @@ class Query(object):
 
 class Mutation(object):
     create_post = CreatePost.Field()
+    delete_post = DeletePost.Field()

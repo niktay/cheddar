97cd71c1460ba68d68803633acd9f69830c88e01
Nikolas Tay <niktay94@gmail.com>
2018-11-15 01:05:03 +0800

    Add tests for user schema

--- cheddar/users/schema.py
+++ cheddar/users/schema.py
@@ -1,5 +1,6 @@
 import graphene
 from django.contrib.auth import get_user_model
+from graphene import InputObjectType
 from graphene_django import DjangoObjectType
 
 
@@ -8,21 +9,29 @@ class UserType(DjangoObjectType):
         model = get_user_model()
 
 
+class CreateUserInput(InputObjectType):
+    username = graphene.String(required=True)
+    password = graphene.String(required=True)
+    first_name = graphene.String(required=True)
+    last_name = graphene.String(required=True)
+    email = graphene.String(required=True)
+
+
 class CreateUser(graphene.Mutation):
     class Arguments:
-        username = graphene.String(required=True)
-        password = graphene.String(required=True)
-        email = graphene.String(required=True)
+        new_user = CreateUserInput(required=True)
 
     user = graphene.Field(UserType)
 
-    def mutate(self, info, username, password, email):
+    def mutate(root, info, new_user=None):
         user = get_user_model()(
-            username=username,
-            email=email,
+            username=new_user.username,
+            email=new_user.email,
+            first_name=new_user.first_name,
+            last_name=new_user.last_name,
         )
 
-        user.set_password(password)
+        user.set_password(new_user.password)
         user.save()
 
         return CreateUser(user=user)
@@ -34,9 +43,9 @@ class Mutation(graphene.ObjectType):
 
 class Query(graphene.ObjectType):
     me = graphene.Field(UserType)
-    users = graphene.List(UserType)
+    all_users = graphene.List(UserType)
 
-    def resolve_users(self, info):
+    def resolve_all_users(self, info):
         return get_user_model().objects.all()
 
     def resolve_me(self, info):
